20bd580c743590f1cdb409b80dbaf126d89e4795
Noah Taggart <noahtag@gmail.com>
2022-07-14 16:51:13 -0500

    freelancer_rates completed

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,7 +1,7 @@
 defmodule FreelancerRates do
   def daily_rate(hourly_rate) do
     # Please implement the daily_rate/1 function
-    hourly_rate * 8
+    hourly_rate * 8 / 1
   end
 
   def apply_discount(before_discount, discount) do
@@ -11,11 +11,12 @@ defmodule FreelancerRates do
 
   def monthly_rate(hourly_rate, discount) do
     # Please implement the monthly_rate/2 function
-    
-    
+    round(Float.ceil(hourly_rate * 8 * 22 * (1.0 - discount/100)))
+
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
     # Please implement the days_in_budget/3 function
+    Float.floor(budget /apply_discount(daily_rate(hourly_rate), discount), 1)
   end
 end
