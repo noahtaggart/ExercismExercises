36cdf53883728a26cdbd823ca420f7564e84fd53
Noah Taggart <noahtag@gmail.com>
2022-07-15 11:27:42 -0500

    darts completed

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,13 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance_squared =  x * x + y * y
+    cond do
+      distance_squared > 100 -> 0
+      distance_squared > 25 -> 1
+      distance_squared > 1 -> 5
+      true -> 10
+
+    end
   end
 end
