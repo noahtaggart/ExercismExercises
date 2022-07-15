90dec3af8eb87058634837cbf6b9b4f3c3d9e0ce
Noah Taggart <noahtag@gmail.com>
2022-07-15 10:39:44 -0500

    secrets

---
+++ elixir/secrets/lib/secrets.ex
@@ -0,0 +1,51 @@
+defmodule Secrets do
+  def secret_add(secret) do
+    # Please implement the secret_add/1 function
+    fn adder ->
+      secret + adder
+    end
+  end
+
+  def secret_subtract(secret) do
+    # Please implement the secret_subtract/1 function
+    fn subtractor ->
+      subtractor - secret
+    end
+  end
+
+  def secret_multiply(secret) do
+    # Please implement the secret_multiply/1 function
+    fn multiplier ->
+      multiplier * secret
+    end
+  end
+
+  def secret_divide(secret) do
+    # Please implement the secret_divide/1 function
+    fn divisor ->
+      div(divisor, secret)
+    end
+  end
+
+  def secret_and(secret) do
+    # Please implement the secret_and/1 function
+    fn ander ->
+      Bitwise.band(ander, secret)
+    end
+  end
+
+  def secret_xor(secret) do
+    # Please implement the secret_xor/1 function
+    fn orer ->
+      Bitwise.bxor(orer, secret)
+    end
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+    # Please implement the secret_combine/2 function
+    fn combine ->
+      secret_function2.(secret_function1.(combine))
+
+    end
+  end
+end
