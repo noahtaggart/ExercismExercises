4167ecaf2f817843530fe1adb61510b180653eaf
Noah Taggart <noahtag@gmail.com>
2022-07-15 16:14:06 -0500

    birdcount

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,23 +1,22 @@
 defmodule BirdCount do
-  def today(list) do
-    List.first(list)
-  end
+  def today([]), do: nil
+  def today([h | _]), do: h
 
   def increment_day_count([]), do: [1]
-  def increment_day_count([head | tail]) do
-    [head + 1 | tail]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
 
-  end
 
-  def has_day_without_birds?(list) do
-    Enum.member?(list, 0)
-  end
+  def has_day_without_birds?([]), do: false
+  def has_day_without_birds?([0 | _]), do: true
+  def has_day_without_birds?([_ | t]), do: has_day_without_birds?(t)
 
-  def total(list) do
-    Enum.sum(list)
-  end
+  def total([]), do: 0
+  def total([h | t]), do: h + total(t)
 
-  def busy_days(list) do
-    Enum.filter(list, fn x -> x >= 5 end) |> length()
-  end
+  def busy_days([]), do: 0
+  def busy_days([h | t]) when h >= 5, do: 1 + busy_days(t)
+  def busy_days([_ | t]), do: busy_days(t)
+
+
+
 end
