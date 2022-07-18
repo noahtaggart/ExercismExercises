d47d0ad7ba4c642341644ccdf67dd840a6ac9452
Noah Taggart <noahtag@gmail.com>
2022-07-18 11:23:46 -0500

    german character conversion

---
+++ elixir/german-sysadmin/lib/username.ex
@@ -0,0 +1,19 @@
+defmodule Username do
+  def sanitize('') do
+  ''
+  end
+  def sanitize([head | tail]) do
+    # ä becomes ae
+    #
+  result = case head do
+    ?ä -> 'ae'
+    ?ö -> 'oe'
+    ?ü -> 'ue'
+    ?ß -> 'ss'
+    x when x >= ?a and x<= ?z -> [x]
+    ?_ -> [?_]
+    _ -> ''
+  end
+  result ++ sanitize(tail)
+  end
+end
