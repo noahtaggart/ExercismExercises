420c764696c376ebf484227cbe2bd28473907869
Noah Taggart <noahtag@gmail.com>
2022-07-26 13:31:36 -0500

    basketball website done

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,22 @@
 defmodule BasketballWebsite do
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    do_extract_from_path(data, keys(path))
   end
 
+  # if path is empty, return data
+  defp do_extract_from_path(data, []), do: data
+
+  # if path isn't empty, splits path and checks against data
+  defp do_extract_from_path(data, [key | rest]) do
+    do_extract_from_path(data[key], rest)
+   end
+
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    get_in(data, keys(path))
+  end
+
+  # splts path on "." and trims out the period
+  defp keys(path) do
+    String.split(path, ".")
   end
 end
